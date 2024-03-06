69aebfcf8679707179428c66bf3f082bdb5a0f50
Mehdi pourhasan <mprhasn@gmail.com>
2024-03-06 10:09:06 +0330

    Initial commit after project completion

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '25304e90-a065-4609-adc3-388e0a2e5f0f';
+export const MODAL_CLOSE_SEC = 2.5;
